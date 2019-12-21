a95e48bf7db012ec0490a2a74ff1c9383844d3ff
ChalutTV <54266833+ChalutTV@users.noreply.github.com>
2019-12-21 21:37:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 client.login(process.env.TOKEN);
+const fs = require('fs');
 
-module.exports = (client) => {
-    client.user.setPresence({
-        game: {
-            name: "c/help --> Affiche l'aide !"
-        }
-    });
-};
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+});
